7bd92044d632e1104ce56063ac3c116c2d0213cc
Miggs <scythe31297@gmail.com>
2025-08-21 23:58:58 +0800

    game.kt update

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,15 @@
-class Game {
-    // Awaiting review
+import kotlin.random.Random
+
+class Game(val cost: Int) {
+
+    fun play(card: Card) {
+        if (card.credits >= cost) {
+            card.credits = card.credits - cost
+            val ticketsWon = Random.nextInt(0, 16)  // 0-15 tickets
+            card.tickets = card.tickets + ticketsWon
+            println("Card #${card.id} won ${ticketsWon} tickets! New total: ${card.tickets} tickets")
+        } else {
+            println("Insufficient balance, please recharge")
+        }
+    }
 }
